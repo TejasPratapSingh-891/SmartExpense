97b08bf35c78d00ec538bdb35d6bf4d055c660ec
Tejas Pratap Singh <tejasprataps891@gmail.com>
2026-09-04 12:34:36 +0530

    chore: set default port to 5001

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app
 app = create_app(os.environ.get('FLASK_CONFIG', 'default'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     print(f"\n=======================================================")
     print(f" SmartExpense Platform is running at:")
     print(f" http://127.0.0.1:{port}")
